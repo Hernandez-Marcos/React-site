1dda0445cffee75d399e9f917791133bb6bb9efa
SgtFreddy713 <captesxel@gmail.com>
2023-08-07 15:28:20 -0300

    Practicing JSX

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
+ //just practicing
+ const bestGames = (
+    <div>
+        <h1>Best games</h1>
+            <h2>Shooters</h2>
+                <ul>
+                    <li>
+                        <p>CS:GO</p>
+                        <p>Call Of Duty</p>
+                    </li>
+                </ul>
+            <h2>Strategy</h2>
+                <ul>
+                    <li>Chess</li>
+                    <li>Supremacy 1914</li>
+                    <li>Poker</li>
+                </ul>
+            
+    </div>
+ )
+
+ ReactDOM.render(
+    bestGames,
+    document.getElementById("root")
+ )
